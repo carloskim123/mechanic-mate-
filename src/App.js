38a84ce8a210b659b5098b17be60f2baa962331d
vinsky001 <ekibet544@gmail.com>
2024-02-08 18:34:22 +0300

    Added header, loginUser, to components

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./components/header";
+// import Login from "./components/loginUser";
+
+const App = () => {
+  return (
+    <div>       
+      <Header />
+      {/* <Login /> */}
+    </div>
+  );
+}
+
+export default App;
