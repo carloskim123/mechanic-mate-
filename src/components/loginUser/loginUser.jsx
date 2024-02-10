28caf9e76733a2276088f74aaf58efa8747c712f
vinsky001 <ekibet544@gmail.com>
2024-02-10 11:59:59 +0300

    Added auth logic that sends a login request to the server

--- src/components/loginUser/loginUser.jsx
+++ src/components/loginUser/loginUser.jsx
@@ -1,18 +1,48 @@
 import React, { useState } from "react";
+import { useNavigate }  from "react-router-dom";
 import { FaUser, FaLock,} from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
 import './loginUser.css';
 
 const Login = () => {
     const [isRegistering, setIsRegistering] = useState(false);
+    const [username, setUsername] = useState('');    
+    const [password, setPassword] = useState('');
+    const navigate = useNavigate();
 
     const toggleRegister = () => {
         setIsRegistering(!isRegistering);
     };
 
+    const handleLogin = async (event) => {
+        event.preventDefault();
+        try {
+            // Send login request to backend server
+            const response = await fetch('/', {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({ username, password })
+            });
+
+            if (response.ok) {
+                // Authentication successful, redirect to dashboard
+                navigate("/dashboard");
+            } else {
+                // Authentication failed, display error message
+                const errorData = await response.json();
+                throw new Error(errorData.message || "Login failed. Please try again.");
+            }
+        } catch (error) {
+            // Handle any errors that occur during login
+            console.error("Login error:", error.message);
+            alert(error.message);
+        }
+    };   
     return (
         <div className="wrapper">
-            <form action="">
+            <form onSubmit={handleLogin}>
                 <h1>{isRegistering ? 'Register' : 'SignIn'}</h1>
                 <div className="input-box">
                     <input type="text" placeholder="Username" required />
