4acc0bc0df070ed88426678b66de740f8edd5639
vinsky001 <ekibet544@gmail.com>
2024-02-12 21:14:40 +0300

    Header componet that opens sidebar

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,12 +1,25 @@
-import React from "react";
+import React from 'react'
+import 
+ {BsFillBellFill, BsFillEnvelopeFill, BsPersonCircle, BsSearch, BsJustify}
+ from 'react-icons/bs'
+ import './App.css'
 
-
-const Header = () => {
-  return (
-    <header>
-      <h1>Mechanic Mate</h1>
+ const Header = ({ OpenSidebar }) => {
+    return (
+    <header className='header'>
+        <div className='menu-icon'>
+            <BsJustify className='icon' onClick={OpenSidebar}/>
+        </div>
+        <div className='header-left'>
+            <BsSearch  className='icon'/>
+        </div>
+        <div className='header-right'>
+            <BsFillBellFill className='icon'/>
+            <BsFillEnvelopeFill className='icon'/>
+            <BsPersonCircle className='icon'/>
+        </div>
     </header>
-  );
-};
+  )
+}
 
 export default Header;
