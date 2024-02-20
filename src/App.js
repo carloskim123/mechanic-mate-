f60c6d24f7437f67122e7c7e97bb9eaa3f4a1b3f
vinsky001 <ekibet544@gmail.com>
2024-02-20 11:27:21 +0300

    Resolved the issue where "useState" was not defined in App.js by importing it from React. Also removed unnecessary eslint-disable-next-line directive. Verified dependencies and ensured proper environment setup. Updated the code comments for clarity

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
-import React from "react";
-import Header from "./components/header";
-// import Login from "./components/loginUser";
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './components/loginUser/loginUser';
+import Dashboard from './components/dashboard/dashboard';
+import Header from './components/header/header';
+import Sidebar from './components/sidebar/sidebar';
 
 const App = () => {
-  return (
-    <div>       
-      <Header />
-      {/* <Login /> */}
+  // eslint-disable-next-line no-undef
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  // Function to handle authentication status
+  const handleAuthentication = (status) => {
+    setIsAuthenticated(status);
+  };
+
+return (
+  <Router>
+    <div>
+      {/* Render Header and Sidebar components for authenticated users */}
+      {isAuthenticated && (
+        <div>
+          <Header />
+          <Sidebar />
+        </div>
+      )}
+
+      <Routes>
+        {/* Define routes for Login and Dashboard */}
+        <Route
+          path="/"
+          element={<Login onAuthentication={handleAuthentication} />}
+        />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
     </div>
+  </Router>
   );
-}
+};
+
 
 export default App;
