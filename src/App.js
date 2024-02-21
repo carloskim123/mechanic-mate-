8e8a374ba361137ee045bc7526e6801d49b9e381
vinsky001 <ekibet544@gmail.com>
2024-02-21 16:53:54 +0300

    Added dashboard to be rendered whenever a user is authenticated

--- src/App.js
+++ src/App.js
@@ -2,41 +2,25 @@ import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/loginUser/loginUser';
 import Dashboard from './components/dashboard/dashboard';
-import Header from './components/header/header';
-import Sidebar from './components/sidebar/sidebar';
 
 const App = () => {
-  // eslint-disable-next-line no-undef
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-  // Function to handle authentication status
   const handleAuthentication = (status) => {
     setIsAuthenticated(status);
   };
 
-return (
-  <Router>
-    <div>
-      {/* Render Header and Sidebar components for authenticated users */}
-      {isAuthenticated && (
-        <div>
-          <Header />
-          <Sidebar />
-        </div>
-      )}
-
+  return (
+    <Router>
       <Routes>
-        {/* Define routes for Login and Dashboard */}
         <Route
           path="/"
           element={<Login onAuthentication={handleAuthentication} />}
         />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/dashboard" element={isAuthenticated ? <Dashboard /> : <Login onAuthentication={handleAuthentication} />} />
       </Routes>
-    </div>
-  </Router>
+    </Router>
   );
 };
-
 
 export default App;
