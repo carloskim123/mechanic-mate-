0b4aa4e4e19b0bdc42eff7ac807a78b83ca9c809
vinsky001 <ekibet544@gmail.com>
2024-02-21 16:51:58 +0300

    imported Header and sidebar componets to the dashboard

--- src/components/dashboard/dashboard.jsx
+++ src/components/dashboard/dashboard.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import Header from '../header/header';
+import Sidebar from '../sidebar/sidebar';
 import './dashboard.css';
 
-const dashboard = () => {
-    return
+const Dashboard = () => {
+    return (
+        <div>
+            <Header />
+            <Sidebar />
+            {/* Rest of your dashboard content goes here */}
+        </div>
+    );
 };
-export default dashboard;
+
+export default Dashboard;
