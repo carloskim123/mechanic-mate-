2b24d62c17aa2a6e11aa0f73cd09ea54e783cba3
vinsky001 <ekibet544@gmail.com>
2024-03-06 18:51:11 +0300

    Added authcontext that checks if a user is authenticated

--- src/components/dashboard/dashboard.jsx
+++ src/components/dashboard/dashboard.jsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useContext, useEffect } from 'react';
+import AuthContext from '../authContext/authContext';
+import { useNavigate } from 'react-router-dom';
 import Header from '../header/header';
 import Sidebar from '../sidebar/sidebar';
 import Main from "../main/main";
 import './dashboard.css';
 
 const Dashboard = () => {
+    const { auth } = useContext(AuthContext);
+    const navigate = useNavigate();
+  
+    useEffect(() => {
+      if(!auth) {
+        navigate('/login')
+      }
+    },[])
     return (
         <div>
             <Header />
