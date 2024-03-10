040725f43b7115fd290780095c13bcb05f7e7574
vinsky001 <ekibet544@gmail.com>
2024-03-10 19:58:52 +0300

    Updated App.js to include new routes and components for customer, car, and service details management

--- src/App.js
+++ src/App.js
@@ -6,6 +6,14 @@ import AuthContext from '../src/components/authContext/authContext';
 import Cookies from 'js-cookie'
 import Root from './components/root';
 import Register from './components/registerUser/registerUser';
+import CreateCustomer from './components/createCustomer/createCustomer';
+import ViewCustomers from './components/viewCustomer/viewCustomers';
+import CreateCars from './components/createCars/createCars';
+import ViewCars from './components/viewCars/viewCars';
+import CreateServiceDetails from './components/createService/createServiceDetails';
+import ViewServiceDetails from './components/viewSeviceDetails/viewServiceDetails';
+import UpdateServiceDetails from './components/updateServiceDetails/updateServiceDetails';
+import DeleteServiceDetails from './components/deleteServiceDetails/deleteServiceDetails';
 
 const App = () => {
   const [auth, setAuth] = useState(false);
@@ -29,6 +37,15 @@ const App = () => {
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path='/create-customer' element={<CreateCustomer />} />
+          <Route path='/view-customer' element={<ViewCustomers />} />
+          <Route path='/create-cars' element={<CreateCars />} />
+          <Route path='/view-cars' element={<ViewCars />} />
+          <Route path='/create-service' element={<CreateServiceDetails />} />
+          <Route path='/view-service' element={<ViewServiceDetails />} />
+          <Route path='/update-service' element={<UpdateServiceDetails />} />
+          <Route path='/delete-service' element={<DeleteServiceDetails />} />
+
         </Routes>
       </Router>
     </AuthContext.Provider>
